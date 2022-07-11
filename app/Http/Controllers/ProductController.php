5afa744a1962830b912b02e232126edf3fd7f0aa
Aleksandr Ostromogilskii <ostromog@mail.ru>
2022-07-11 14:51:54 +0300

    create controller

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{   
+    //
+    public function get(int $id)
+    {
+
+    }
+
+    //
+    public function getAll()
+    {
+
+    }
+
+    //
+    public function create(string $name, string $description, float $price, string $main_photo, string $photos)
+    {
+
+    }
+}

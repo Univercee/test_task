968a4f8c31e8c9d1e361ef75134d577b9f704532
Aleksandr Ostromogilskii <ostromog@mail.ru>
2022-07-11 19:52:36 +0300

    add routes

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,8 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::prefix('products')->group(function(){
+    Route::get('/', 'ProductController@getAll');
+    Route::get('/{id}', 'ProductController@get');
+    Route::post('/create', 'ProductController@create');
 });

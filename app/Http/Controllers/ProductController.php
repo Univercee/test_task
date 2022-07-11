afc6433954337eeb399882d9e2cbdf4403ff58f3
Aleksandr Ostromogilskii <ostromog@mail.ru>
2022-07-11 19:52:19 +0300

    update controller

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Model\Product;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
@@ -10,18 +11,28 @@ class ProductController extends Controller
     //
     public function get(int $id)
     {
-
+        $product = Product::get($id);
+        return response()->json($product, 200);
     }
 
     //
     public function getAll()
-    {
-
+    {   
+        $all_products = Product::getAll();
+        return response()->json($all_products, 200);
     }
 
     //
-    public function create(string $name, string $description, float $price, string $main_photo, string $photos)
+    public function create(Request $request)
     {
-
+        $id = Product::create(
+            $request->input('name')??'', 
+            $request->input('description')??'', 
+            $request->input('price')??-1, 
+            $request->input('main_image')??'',
+            $request->input('images')??'[]'
+        );
+        $code = $id==-1 ? 422 : 200;
+        return response()->json($id, $code);
     }
 }

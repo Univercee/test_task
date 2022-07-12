11151ea50124a04141669aa14828ab9b588f8426
Aleksandr Ostromogilskii <ostromog@mail.ru>
2022-07-12 17:31:33 +0300

    create product list component

--- resources/js/components/ProductList.vue
+++ resources/js/components/ProductList.vue
@@ -1,22 +1,49 @@
 <template>
     <div>
-    <form method="post" action='http://127.0.0.1:8000/api/products/create'>
-            <input name="name" value="test">
-            <input name="description" value="test desc">
-            <input name="price" value="0">
-            <input name="main_image" value="aaa">
-            <input name="images[0]" value="bbb">
-            <input name="images[1]" value="ccc">
-            <input type="submit">
-        </form>
+        <button v-on:click="sortByCreatedAt">CreatedAt</button>
+        <button v-on:click="sortByPrice">Price</button>
+        <div v-for="product in products" :key="product.id" class="card">
+            <p>{{product.name}}</p>
+            <p>{{product.price}}</p>
+            <p>{{product.main_image}}</p>
+            <p>{{product.created_at}}</p>
+            <router-link :to="{name: 'product', params:{id: product.id}}">Go to About</router-link>
+        </div>
     </div>
 </template>
 
 <script>
     export default {
         name: "ProductList",
+        data(){
+            return {
+                products: []
+            }
+        },
         mounted() {
-            console.log('Component mounted.')
+            this.GET_PRODUCT_LIST()
+        },
+        methods: {
+            async GET_PRODUCT_LIST(){
+                await axios({
+                    method: 'get',
+                    url: 'api/products',
+                }).then(response => this.products = response.data);
+            },
+            sortByCreatedAt(){
+                this.products.sort(function(a, b) {
+                    var x = a.created_at; 
+                    var y = b.created_at;
+                    return ((x < y) ? -1 : ((x > y) ? 1 : 0));
+                });
+            },
+            sortByPrice(){
+                this.products.sort(function(a, b) {
+                    var x = a.price; 
+                    var y = b.price;
+                    return ((x < y) ? -1 : ((x > y) ? 1 : 0));
+                });
+            }
         }
     }
 </script>

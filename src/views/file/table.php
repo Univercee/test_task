441389e80ac2ee832f10979a6ba47a891a4d2986
u7NGAQhsej <aleksandr.ostromogilskii@gmail.com>
2024-02-13 19:48:45 +0400

    update styles

--- src/views/file/table.php
+++ src/views/file/table.php
@@ -4,42 +4,44 @@
     $files = $state->data;
     if($files && count($files)){
         ?>
-        <table>
-            <thead>
-                <tr>
-                    <th>Name</th>
-                    <th></th>
-                    <th></th>
-                    <th></th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php
-                    /**
-                     * @var string $filename
-                     */
-                    foreach($state->data as $filename){
-                    ?>
-                        <tr>
-                            <td><?php echo $filename?></td>
-                            <td>
-                                <a href="/file?filename=<?php echo $filename?>"> Просмотр</a>
-                            </td>
-                            <td>
-                                <a href="/file-send-action?filename=<?php echo $filename;?>">Отправить</a>
-                            </td>
-                            <td>
-                                <form action="file-delete-action" method="POST">
-                                    <input type="text" name="filename" value="<?php echo $filename;?>" hidden>
-                                    <input type="submit" value="Удалить">
-                                </form>
-                            </td>
-                        </tr>
+        <div class="table-wrapper">
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th></th>
+                        <th></th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
                     <?php
-                    }
-                ?>
-            </tbody>
-        </table>
+                        /**
+                         * @var string $filename
+                         */
+                        foreach($state->data as $filename){
+                        ?>
+                            <tr>
+                                <td><?php echo $filename?></td>
+                                <td>
+                                    <a href="/file?filename=<?php echo $filename?>"> Просмотр</a>
+                                </td>
+                                <td>
+                                    <a href="/file-send-action?filename=<?php echo $filename;?>">Отправить</a>
+                                </td>
+                                <td>
+                                    <form action="file-delete-action" method="POST">
+                                        <input type="text" name="filename" value="<?php echo $filename;?>" hidden>
+                                        <input type="submit" value="Удалить">
+                                    </form>
+                                </td>
+                            </tr>
+                        <?php
+                        }
+                    ?>
+                </tbody>
+            </table>
+        </div>
         <?php
     }
     else {
--- src/views/user/table.php
+++ src/views/user/table.php
@@ -4,42 +4,44 @@
 
     if($state->data && count($state->data)){
         ?>
-        <table>
-            <thead>
-                <tr>
-                    <th>Name</th>
-                    <th>Email</th>
-                    <th>Telegram ID</th>
-                    <th></th>
-                    <th></th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php
-                    /**
-                     * @var UserModel $user
-                     */
-                    foreach($state->data as $user){
-                    ?>
-                        <tr>
-                            <td><?php echo $user->getName()?></td>
-                            <td><?php echo $user->getEmail()?></td>
-                            <td><?php echo $user->getTelegramId()?></td>
-                            <td>
-                                <a href="/user-update?id=<?php echo $user->getId();?>">Редактировать</a>
-                            </td>
-                            <td>
-                                <form action="user-delete-action" method="POST">
-                                    <input type="text" name="id" value="<?php echo $user->getId(); ?>" hidden>
-                                    <button>Удалить</button>
-                                </form>
-                            </td>
-                        </tr>
+        <div class="table-wrapper">
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Telegram ID</th>
+                        <th></th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
                     <?php
-                    }
-                ?>
-            </tbody>
-        </table>
+                        /**
+                         * @var UserModel $user
+                         */
+                        foreach($state->data as $user){
+                        ?>
+                            <tr>
+                                <td><?php echo $user->getName()?></td>
+                                <td><?php echo $user->getEmail()?></td>
+                                <td><?php echo $user->getTelegramId()?></td>
+                                <td>
+                                    <a href="/user-update?id=<?php echo $user->getId();?>">Редактировать</a>
+                                </td>
+                                <td>
+                                    <form action="user-delete-action" method="POST">
+                                        <input type="text" name="id" value="<?php echo $user->getId(); ?>" hidden>
+                                        <button>Удалить</button>
+                                    </form>
+                                </td>
+                            </tr>
+                        <?php
+                        }
+                    ?>
+                </tbody>
+            </table>
+        </div>
         <?php
     }
     else {
